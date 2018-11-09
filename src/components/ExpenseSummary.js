448a054dd3d8d4efa3f9e53f4ef4163c03c8b4c3
patrick@beatnole.com <patrick@beatnole.com>
2018-11-09 08:48:06 -0500

    Setup remove expense from store and firebase

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -4,23 +4,25 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import { getExpensesTotal } from '../selectors/expense-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, allExpenseCount }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
   
   return (
     <div>
-      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+      <h1>Viewing {expenseCount} {expenseWord} of {allExpenseCount} totalling {formattedExpensesTotal}</h1>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  const allExpenses = selectExpenses(state.expenses, { text: '', sortBy: 'date' , startDate: 0, })
 
   return {
     expenseCount: visibleExpenses.length,
-    expensesTotal: getExpensesTotal(visibleExpenses)
+    expensesTotal: getExpensesTotal(visibleExpenses),
+    allExpenseCount: allExpenses.length
   };
 };
 
