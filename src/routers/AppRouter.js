0a76e222399f4bd0ca3ab6b0e455ca255010c3bc
patrick@beatnole.com <patrick@beatnole.com>
2018-11-09 14:55:13 -0500

    setup public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
 import Login from '../components/Login'
-import HelpPage from '../components/HelpPage.js';
 import AddExpensePage from '../components/AddExpensePage.js';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
 import EditExpensePage from '../components/EditExpensePage.js';
 import NotFoundPage from '../components/NotFoundPage.js';
 import PrivateRoute from './PrivateRoute'
-
+import PublicRoute from './PublicRoute'
 export const history = createHistory()
 
 const AppRouter = () => (
     <Router history={history}>
     <div>
         <Switch>
-            <Route path="/" component={Login} exact={true}/>
+            <PublicRoute path="/" component={Login} exact={true}/>
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
             <PrivateRoute path="/create" component={AddExpensePage} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage}  />
         </Switch>
     </div>
